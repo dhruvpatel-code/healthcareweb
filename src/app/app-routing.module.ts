31984dd371e5016c6e5e6e3f5bbf24d69b145a92
Dhruv Patel <dhruvpatelcode@gmail.com>
2023-07-10 20:07:12 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientListComponent } from './patients/patient-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'patient', component: PatientListComponent },
+  { path: '', redirectTo: 'patient', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: 'patient' } // Handle unknown routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
